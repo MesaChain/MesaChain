4bbcfe54e19ab238cbf716028b1adafa8c4c75bd
victorbuikem <chibuikemkalu466@gmail.com>
2025-03-24 06:23:53 +0100

    feat: update frontend configuration and dependencies
    - Added Radix UI components: `@radix-ui/react-slot` and `@radix-ui/react-tabs` to package.json.
    - Updated pnpm-lock.yaml with new Radix UI dependencies and their versions.
    - Modified components.json to adjust the path for global CSS.
    - Updated tsconfig.json to specify paths for module resolution.
    - Removed unused globals.css, layout.tsx, page.tsx, and utils.ts files.

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='text-3xl bg-background' >{children}</body>
     </html>
   )
 } 
---
+++ apps/frontend/src/app/menu/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page () {
+    return (
+        <div className="bg-red-400">
+            jnkscdclmdmkm
+        </div>
+    )
+}
